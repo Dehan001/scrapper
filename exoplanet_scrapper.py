d5e393ff27410d02030d04829d503429940decb5
Dehan001 <dehannoor3199@gmail.com>
2024-09-21 01:15:43 +0600

    31 done

--- exoplanet_scrapper.py
+++ exoplanet_scrapper.py
@@ -47,8 +47,8 @@ def main():
     exoplanet_content = []
     webdriver_path = r"C:\Program Files (x86)\chromedriver.exe"
     service = Service(webdriver_path)
-    count = 27
-    for page_id in range(261, 351):
+    count = 31
+    for page_id in range(301, 351):
         driver = webdriver.Chrome(service=service)
         url = f"https://science.nasa.gov/exoplanets/exoplanet-catalog/?pageno={page_id}&content_list=true"
         driver.get(url)

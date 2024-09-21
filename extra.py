a0e375d098332442b0b87443823c8b9f4165670d
Dehan001 <dehannoor3199@gmail.com>
2024-09-22 00:54:28 +0600

    almost finished

--- extra.py
+++ extra.py
@@ -7,39 +7,62 @@ import pandas as pd
 
 columns = ["Planet Name", "Parsecs from Earth", "Planet Mass", "Stellar Magnitude", "Discovery Date", "Planet Radius", "Planet Type", "Discovery Method", "Orbital Radius", "Orbital Period", "Eccentricity"]
 
-
-
 def main():
     exoplanet_content = []
     webdriver_path = r"C:\Program Files (x86)\chromedriver.exe"
     service = Service(webdriver_path)
     driver = webdriver.Chrome(service=service)
-    for page_id in range(70, 261):
+    count = 12
+    for page_id in range(111, 121):
         
         url = f"https://science.nasa.gov/exoplanets/exoplanet-catalog/?pageno={page_id}&content_list=true"
         driver.get(url)
         
         try:
             exoplanets = driver.find_elements(By.CLASS_NAME, "hds-content-item")
-
-            for exoplanet in exoplanets:
+            data=[exoplanet.text for exoplanet in exoplanets]
+            
+                
+            for datum in data:
                 contents = {}
-                lines = exoplanet.text.split('\n')
+                lines = datum.split('\n')
                 _,contents["Parsecs from Earth"] = lines[1].split(': ', 1)
                 contents["Planet Name"] = lines[0]
                 for line in lines:
                     if ': ' in line:
                         key, value = line.split(': ', 1)  # Split into key and value, limit to 1 split
                         contents[key.strip()] = value.strip()
-
+                
+                try: 
+                    url2_planets = f"https://science.nasa.gov/exoplanet-catalog/{contents.get('Planet Name', '').replace(' ', '-').replace('.', '').lower()}/"
+                    driver.get(url2_planets)
+                    planet_info_blocks = driver.find_elements(By.CLASS_NAME, "smd-acf-grid-col")
+                except:
+                    url2_planets = f"https://science.nasa.gov/exoplanet-catalog/{contents.get('Planet Name', '').replace(' ', '-').replace('.', '-').lower()}/"
+                    driver.get(url2_planets)
+                    planet_info_blocks = driver.find_elements(By.CLASS_NAME, "smd-acf-grid-col")
+                
+                for block in planet_info_blocks:
+                    label_element = block.find_element(By.CLASS_NAME, "text-bold")
+                    value_element = block.find_element(By.TAG_NAME, "span")
+                    label = label_element.text.strip(": ").replace(':', "").strip()
+                    value = value_element.text.strip()
+                    contents[label] = value
+                
+                
                 exoplanet_content.append(contents)
 
         except Exception as e:
             print(f"An error occurred on page {page_id}: {e}")
         
-    driver.quit()   
-    df = pd.DataFrame(data=exoplanet_content, columns=columns)
-    df.to_csv(f'nasa_exoplanets_extra.csv', index=False)    
+        
+        if (page_id%10==0 or page_id==384):
+            print(len(exoplanet_content))
+            df = pd.DataFrame(data=exoplanet_content, columns=columns)
+            df.to_csv(f'nasa_exoplanets_part{count}.csv', index=False)
+            exoplanet_content=[]
+            count += 1
+    driver.quit()
 
 if __name__ == "__main__":
     main()
